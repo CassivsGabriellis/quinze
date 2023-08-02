d94420818b66d60166b7291872ce3d3db47e07f3
Santunis Oliveira <santunis.oliveira@gmail.com>
2023-08-02 11:06:36 -0300

    commit inicial

---
+++ src/main/java/br/com/quinze/QuinzeApplication.java
@@ -0,0 +1,13 @@
+package br.com.quinze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuinzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuinzeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/quinze/QuinzeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.quinze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuinzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
