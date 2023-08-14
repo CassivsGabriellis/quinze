d01a5aa571d42078fd72390b6f2a6dc604d42885
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-08-14 12:04:36 -0300

    Refatoração do controle de Exceções

--- src/main/java/br/com/quinze/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/quinze/exception/GlobalExceptionHandler.java
@@ -6,16 +6,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException() {
+    public ResponseEntity<String> handleException(Exception e) {
+        logger.error("Erro interno no servidor:", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocorreu um erro interno no servidor.");
     }
 
     @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
     public ResponseEntity<String> handleNotFoundException(ChangeSetPersister.NotFoundException e) {
+        logger.error("Recurso não encontrado:", e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 }
---
+++ src/test/java/br/com/quinze/controller/CombinationControllerTest.java
@@ -0,0 +1,2 @@
+package br.com.quinze.controller;public class CombinationControllerTest {
+}
